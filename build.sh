c1285dd6b933822f12cbf35754835b020177b437
Susheel Yadav <sushyad@gmail.com>
2016-11-04 15:36:33 -0400

    added environment properties file and build file

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+source setenv.sh
+
+OTA_PASS_MD5_HASH=($(echo $OTA_PASS | md5sum))
+export PLATFORMIO_SRC_BUILD_FLAGS="'-DSTA_PASS=\"$WIFI_PASS\"' '-DSTA_SSID=\"$WIFI_SSID\"' '-DMQTT_HOST=\"$MQTT_HOST\"' '-DOTA_HOST=\"$OTA_HOST\"' '-DOTA_PASS_MD5_HASH=\"$OTA_PASS_MD5_HASH\"' '-DPROJECT_NAME=\"$PROJECT_NAME\"' '-DVERSION_BIN=\"$VERSION\"'"
+
+if [ $# -eq 0 ]
+then
+    FILENAME=${PROJECT_NAME}-${VERSION}.bin
+    echo "Compiling version $VERSION as a binary ($FILENAME)."
+    platformio run
+    retval=$?
+    if [ $retval -eq 0 ]; then
+      cp .pioenvs/esp8266/firmware.bin $FILENAME
+    fi
+else
+    if [ $1 == "usb" ]
+    then
+        echo "Compiling version $VERSION and flashing via USB."
+        platformio run --target upload
+        retval=$?
+    else
+        echo "Compiling version $VERSION and flashing via OTA to $1."
+        export PLATFORMIO_UPLOAD_FLAGS="-a $OTA_PASS"
+        platformio run --target upload --upload-port=$1
+        retval=$?
+    fi
+fi
+
+if [ $retval -eq 0 ]; then
+    # Auto-increment the version in this script.
+    sed -i -re "s/VERSION=\"[0-9]+\"/VERSION=\"$(( ++VERSION ))\"/" setenv.sh
+fi
